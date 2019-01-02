14491528e6fcf462e6f23d66cdb34449e96ff1d0
Neo <neo.caixiaoqing@gmail.com>
2019-01-02 16:28:50 +0800

    create project

---
+++ DemoCollection/DemoCollection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoCollection
+//
+//  Created by Neo on 2019/1/2.
+//  Copyright © 2019 Neo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoCollection/DemoCollection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoCollection
+//
+//  Created by Neo on 2019/1/2.
+//  Copyright © 2019 Neo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
